6298051b4e006c2cab475390967487e3d6a5f6d3
iraelti <60797024+iraelti@users.noreply.github.com>
2020-06-08 16:55:55 -0700

    p03

--- ControlInventario/ControlInventario/Models/proveedores.cs
+++ ControlInventario/ControlInventario/Models/proveedores.cs
@@ -9,14 +9,19 @@ namespace ControlInventario.Models
     public class proveedores
     {
         [Key]
+        [Display(Name = "Código")]
         public int supplierId { get; set; }
+        [Display(Name = "Identificación")]
         public string supplierCode { get; set; }
+        [Display(Name = "Nombre")]
         public string supplierName { get; set; }
 
+        [Display(Name = "Correo electrónico")]
         public string Email { get; set; }
+        [Display(Name = "Teléfono")]
         public string Phone { get; set; }
-        public string Address { get; set; }
-
+        [Display(Name = "Dirección")]
+        public string Address { get; set; }        
 
     }
 }
