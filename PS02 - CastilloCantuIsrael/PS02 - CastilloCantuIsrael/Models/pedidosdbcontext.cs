6709f088d3569e280f42556711d2114abae33da7
iraelti <60797024+iraelti@users.noreply.github.com>
2020-06-09 01:01:54 -0700

    practicasemanal1

---
+++ PS02 - CastilloCantuIsrael/PS02 - CastilloCantuIsrael/Models/pedidosdbcontext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PS02.CastilloCantu.Models
+{
+    public class pedidosdbcontext:DbContext
+    {
+        public pedidosdbcontext() : base("MyConnection")
+        {
+        }
+        public DbSet<pedidos> Pedidos { get; set; }
+
+        public System.Data.Entity.DbSet<PS02.CastilloCantu.Models.productos> productos { get; set; }
+    }
+}
---
+++ PS02 - CastilloCantuIsrael/PS02 - CastilloCantuIsrael/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PS02.CastilloCantu.Startup))]
+namespace PS02.CastilloCantu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
